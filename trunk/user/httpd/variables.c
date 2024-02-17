3f81389b3eaf3639422a6ee45915b7b6f8817565
qzxwm <160295425+qzxwm@users.noreply.github.com>
2024-02-17 23:51:16 +0800

    Update variables.c

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -991,6 +991,7 @@
 			{"zerotier_id", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zerotier_nat", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zero_staticnum_x", "", NULL, EVM_RESTART_ZEROTIER},
+			{"zerotiermoon_enable", "", NULL, EVM_RESTART_ZEROTIER},
 			{"ZeroList", "Group", ARGV((char*)variables_ZeroConf_ZeroList, "8", "55", "zero_staticnum_x"), EVM_RESTART_ZEROTIER},
 			{0,0,0,0}
 	};
